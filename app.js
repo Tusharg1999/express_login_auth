aac3c55c1a9bbb52f1edf94ba5dacbef109433d3
Tusharg1999 <tushargoyal414@gmail.com>
2019-07-11 02:27:42 +0530

    login

---
+++ app.js
@@ -0,0 +1,12 @@
+const express=require('express')
+const expressLayouts=require('express-ejs-layouts')
+const app=express()
+//EJS
+app.use(expressLayouts)
+app.set('view engine','ejs')
+
+const PORT=process.env.PORT ||5000
+app.use('/',require('./routes/index'))
+app.use('/users',require('./routes/users'))
+
+app.listen(PORT,console.log('on '+PORT));
