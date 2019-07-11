40abffe079b0cf698777333e9aa404556f75aece
Tusharg1999 <tushargoyal414@gmail.com>
2019-07-11 20:51:10 +0530

    users can register

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express=require('express')
 const expressLayouts=require('express-ejs-layouts')
+const mongoose=require('mongoose')
 const app=express()
+const PORT=process.env.PORT || 5000
+//db config
+const db=require('./config/keys').mongoURI;
+//connect mongo
+mongoose.connect(db,{useNewUrlParser:true})
+.then(console.log("database connected"))
+.catch(err=> console.log(err))
 //EJS
 app.use(expressLayouts)
 app.set('view engine','ejs')
+//bodyParser
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
 
-const PORT=process.env.PORT ||5000
+
 app.use('/',require('./routes/index'))
 app.use('/users',require('./routes/users'))
 
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,75 @@
 const router=require('express').Router()
+const User=require('../member')
+const bcrypt=require('bcryptjs')
 
 router.get('/login',(req,res)=>{
     res.render('login')
 })
 router.get('/register',(req,res)=>{
     res.render('register')
+})
+router.post('/register',(req,res)=>{
+    const {name,email,password,password2}=req.body
+     let errors=[];
+     //check for fields
+     if(!name || !email || !password || !password2){
+         errors.push({msg:'please fill complete form'})
+     }     
+     if(password!==password2)
+     {
+         errors.push({msg:'password do not matched'})
+     }
+     if(password.length<6)
+     {
+         errors.push({msg:'password should be of 6 characters'})
+     }
+     if(errors.length>0)
+     {
+        res.render('register',{
+            errors,
+            name,
+            email,
+            password,
+            password2
+        })
+     }
+     else{
+         //validation done
+         User.findOne({email:email})
+         .then(user=>{
+             if(user)
+             {
+                 errors.push({msg:'user already exists'})
+                 res.render('register',{
+                    errors,
+                    name,
+                    email,
+                    password,
+                    password2
+                })
+             }
+             else
+             {
+                const user= new User({
+                    name,
+                    email,
+                    password
+                });
+                //password encryption with bcrypt
+                bcrypt.genSalt(10,(err,salt)=>{
+                    bcrypt.hash(user.password,salt,(err,hash)=>{
+                        if(err) throw err;
+                        //save user
+                        user.password=hash;
+                        user.save()
+                        .then(
+                            res.redirect('/users/login')
+                        )
+                        .catch(err=>console.log(err))
+                    })
+                })
+             }
+         })
+     }
 })
 module.exports=router
